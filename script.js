29d55e773a4cc0e604db8cc98a79909656f5e6fc
ERIK PETERSON <ERIK412CLASS@gmail.com>
2026-08-29 19:59:21 -0500

    Form & JS work

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const now = new Date();
+const dateContainer = document.getElementById(`current-date`);
+const greetingContainer = document.getElementById(`greeting`);
+
 //Convert date to Day, Month, ##, ####
 function longDate(currentDate) {
   return (formattedDate = Intl.DateTimeFormat("en-US", {
@@ -20,26 +24,48 @@ function getGreeting(currentTime) {
 }
 
 //Set initial header date & greeting
-const dateContainer = document.getElementById(`current-date`);
-const greetingContainer = document.getElementById(`greeting`);
 
-function updateHeaderGreeting() {
-  const now = new Date();
+let greetingPerson = "friend";
+function updateHeaderGreeting(today) {
   dateContainer.textContent = longDate(now);
-  const greeting = getGreeting(now);
-  greetingContainer.textContent = `Good ${greeting}, friend.`;
+  greeting = getGreeting(now);
+  greetingContainer.textContent = `Good ${greeting}, ${greetingPerson}.`;
 }
 
-updateHeaderGreeting();
+updateHeaderGreeting(now);
 
 //Set intervals for date & greeting
 setInterval(() => {
   const now = new Date();
-  const greeting = getGreeting(now);
-  greetingContainer.textContent = `Good ${greeting}, friend.`;
-}, 1000);
+  dateContainer.textContent = longDate(now);
+}, 60000);
 
 setInterval(() => {
   const now = new Date();
-  dateContainer.textContent = longDate(now);
-}, 60000);
+  const greeting = getGreeting(now);
+  greetingContainer.textContent = `Good ${greeting}, ${greetingPerson}.`;
+}, 1000);
+
+//signup form control
+const step1 = document.getElementById("step1");
+const step2 = document.getElementById("step2");
+
+function nextStep() {
+  const now = new Date();
+  let firstNameField = document.getElementById("firstName");
+  let lastNameField = document.getElementById("lastName");
+  let firstName = firstNameField.value;
+  let lastName = lastNameField.value;
+
+  greetingPerson =
+    firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+  firstName = firstName.trim().toUpperCase();
+
+  lastName = lastName.trim().toUpperCase();
+  step1.classList.add("hidden");
+  step2.classList.remove("hidden");
+
+  const greeting = getGreeting(now);
+  greetingContainer.textContent = `Good ${greeting}, ${greetingPerson}.`;
+  console.log(firstName, lastName);
+}
